efd907298c25943179498bc6f3d45d746c31a5ee
NikhilKukreja26 <kukrejanikhil25@gmail.com>
2023-10-27 14:25:07 +0530

    [feature] - Counter Testing - SetUp Method

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -2,15 +2,26 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:unit_test/counter.dart';
 
 void main() {
+  late Counter counter;
+
+  setUp(() {
+    counter = Counter();
+  });
+
   // * Given When Then
 
   // * PreTest
-  setUp(() => null);
-  setUpAll(() => null);
+  // setUp(() => null);
+  // setUpAll(() => null);
+
+  // * setUp is called before every test
+  // * setUpAll is called before all the tests
+
+  // * setUp -> test -> setUp -> test -> setUp -> test
+  // * setUpAll -> test -> test -> test
 
   // * Testing
   group('Counter Class -', () {
-    final Counter counter = Counter();
     test(
       'Given Counter Class When It Is Instantiated Then Value Of Count Should Be 0',
       () {
@@ -37,9 +48,40 @@ void main() {
         expect(value, 1); //* Real value and Guessing Value
       },
     );
+
+    test(
+      'Given Counter Class When It Is Decremented Then Value Of Count Should Be -1',
+      () {
+        // * Arrange
+
+        // * Act
+        counter.decrementCounter();
+        final value = counter.count;
+
+        // * Assert
+        expect(value, -1); //* Real value and Guessing Value
+      },
+    );
+
+    test(
+      'Given Counter Class When It Is Reset Then Value Of Count Should Be 0',
+      () {
+        // * Arrange
+
+        // * Act
+        counter.resetCounter();
+        final value = counter.count;
+
+        // * Assert
+        expect(value, 0); //* Real value and Guessing Value
+      },
+    );
   });
 
   // * Post Test
-  tearDown(() => null);
-  tearDownAll(() => null);
+  // tearDown(() => null);
+  // tearDownAll(() => null);
+
+  // * test -> TearDown -> test -> TearDown -> test -> TearDown
+  // * test -> test -> test -> TearDownAll
 }
